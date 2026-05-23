741bad641a6655d54f519354d13aefabf873ed01
Faruk-Abdullahi <farafuka78@gmail.com>
2026-05-23 15:55:46 +0100

    Initial commint: Hi!

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
+// Assignment: Build and Deploy your API
+
 const express = require('express')
 const app = express()
 const port = 3000
-
+const path = require('path');
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
 
-// GET /user
-app.get("/user", (req, res) => {
+// 1. GET /user
+app.get("/", (req, res) => {
   res.send("My week 2 API!");
 });
 
 
+// 2. POST /user
+
+app.use(express.json());
 
 app.post('/user', (req, res) => {
-    // Fallback to an empty object {} if req.body is missing or undefined
-    const { name, email } = req.body || {} 
-
-    if (!name || !email) {
-        return res.status(400).json({ 
-            error: "Bad Request", 
-            message: "Missing 'name' or 'email' in JSON payload, or missing Content-Type header." 
-        });
-    }
-
-    res.send(`Hello, ${name}!`);
+    const { name, email } = req.body;
+    
+// Respond with the greeting
+    res.status(200).send(`Hello, ${name}`);
 });
 
 
@@ -38,6 +36,11 @@ app.get('/user/:id', (req, res) => {
 
 
 // 4. .env for PORT
+// 1. Initialize environment variables from .env immediately
+require('dotenv').config();
+
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server successfully running on port ${PORT}`);
